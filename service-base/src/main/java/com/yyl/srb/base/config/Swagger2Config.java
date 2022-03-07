d3ed8a9104d4de915d2f96556fd9727048ca3e39
the-anyan <story_2020@163.com>
2022-03-07 17:03:36 +0800

    2022年3月7日17:03:29

---
+++ service-base/src/main/java/com/yyl/srb/base/config/Swagger2Config.java
@@ -0,0 +1,38 @@
+package com.yyl.srb.base.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@EnableWebMvc
+public class Swagger2Config {
+
+    @Bean
+    public Docket adminApiConfig(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("adminApi")
+                .apiInfo(adminApiInfo())
+                .select()
+                .paths(Predicates.and(PathSelectors.regex("/admin/.*")))
+                .build();
+    }
+    private ApiInfo adminApiInfo(){
+        return new ApiInfoBuilder()
+                .title("srb后台管理系统-API文档")
+                .description("本文档描述了srb后台管理系统接口")
+                .version("1.0")
+                .contact(new Contact("yyl","http://yyl.com","story_2020@163.com"))
+                .build();
+
+    }
+}
